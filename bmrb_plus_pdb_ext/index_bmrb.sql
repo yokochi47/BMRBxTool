1c5cac09c60b4b2d1b755bc20dfefc1f19dd79ff
Masashi Yokochi <yokochi47@gmail.com>
2018-04-12 18:59:01 +0900

    Fix BMRB+PDB index scripts

--- bmrb_plus_pdb_ext/index_bmrb.sql
+++ bmrb_plus_pdb_ext/index_bmrb.sql
@@ -1,24 +1,15 @@
---
--- PostgreSQL database
---
---drop index if exists atom_site_index;
-create index atom_site_index on "Atom_site" ( "Entry_ID" );
 
---drop index if exists gen_dist_constraint_index;
-create index gen_dist_constraint_index on "Gen_dist_constraint" ( "Entry_ID" );
+create index if not exists atom_site_index on "Atom_site" ( "Entry_ID" );
 
---drop index if exists gen_dist_constraint_commnet_org_index;
-create index gen_dist_constraint_comment_org_index on "Gen_dist_constraint_comment_org" ( "Entry_ID" );
+create index if not exists gen_dist_constraint_index on "Gen_dist_constraint" ( "Entry_ID" );
 
---drop index if exists rdc_index;
-create index rdc_index on "RDC" ( "Entry_ID" );
+create index if not exists gen_dist_constraint_comment_org_index on "Gen_dist_constraint_comment_org" ( "Entry_ID" );
 
---drop index if exists rdc_constraint_commnet_org_index;
-create index rdc_constraint_commnet_org_index on "RDC_constraint_comment_org" ( "Entry_ID" );
+create index if not exists rdc_index on "RDC" ( "Entry_ID" );
 
---drop index if exists torsion_angle_constraint_index;
-create index torsion_angle_constraint_index on "Torsion_angle_constraint" ( "Entry_ID" );
+create index if not exists rdc_constraint_commnet_org_index on "RDC_constraint_comment_org" ( "Entry_ID" );
 
---drop index if exists ta_constraint_comment_org_index;
-create index ta_constraint_comment_org_index on "TA_constraint_comment_org" ( "Entry_ID" );
+create index if not exists torsion_angle_constraint_index on "Torsion_angle_constraint" ( "Entry_ID" );
+
+create index if not exists ta_constraint_comment_org_index on "TA_constraint_comment_org" ( "Entry_ID" );
 
