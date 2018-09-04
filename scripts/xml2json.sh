d1ff235ac9153086295d569d01bdb94b520d229c
yokochi47 <yokochi47@gmail.com>
2018-09-04 11:24:57 +0900

    Update xsd2pgschema

--- scripts/xml2json.sh
+++ scripts/xml2json.sh
@@ -59,8 +59,8 @@ JSON_SCHEMA=schema/bmrb_clone.json
 
 java -cp extlibs/xsd2pgschema.jar xsd2jsonschema --xsd $XSD_SCHEMA --col-json --discarded-doc-key-name entry_id --json $JSON_SCHEMA
 
-sed -i -e "3,3 s/\.xsd/\.json/" $JSON_SCHEMA
-sed -i -e "4,5 s/BMRB\/XML/BMRB\/JSON/g" $JSON_SCHEMA
+#sed -i -e "3,3 s/\.xsd/\.json/" $JSON_SCHEMA
+#sed -i -e "4,5 s/BMRB\/XML/BMRB\/JSON/g" $JSON_SCHEMA
 
 echo
 echo "Do you want to update BMRB/JSON? (y [n]) "
