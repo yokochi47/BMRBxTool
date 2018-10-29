4d3b09da8406364ba6b5af7181555f37034bfff3
Masashi Yokochi <yokochi47@gmail.com>
2018-10-29 12:14:57 +0900

    Merge

--- bmr_src/BMRxTool_Java.java
+++ bmr_src/BMRxTool_Java.java
@@ -66,8 +66,8 @@ public class BMRxTool_Java {
 
 		String query_key = (table_name.equals("Entry") ? "ID" : "Entry_ID");
 
-		boolean _integer = has_integer(node) || has_enum_integer(node); // has xsd:integer
-		boolean _integer_id = has_integer_id(node); // has xsd:integer with name "id"
+		boolean _integer = has_integer(node) || has_enum_integer(node); // has xsd:int
+		boolean _integer_id = has_integer_id(node); // has xsd:int with name "id"
 		boolean _decimal = has_decimal(node) || has_enum_decimal(node); // has xsd:decimal
 		boolean _date = has_date(node); // has xsd:date
 		boolean _enum = has_enum(node); // has xsd:restriction
@@ -2474,7 +2474,7 @@ public class BMRxTool_Java {
 
 					Node attr_name = node_map.getNamedItem("type");
 
-					if (attr_name != null && attr_name.getTextContent().equals("xsd:integer"))
+					if (attr_name != null && attr_name.getTextContent().equals("xsd:int"))
 						return true;
 				}
 			}
@@ -2503,7 +2503,7 @@ public class BMRxTool_Java {
 
 						attr_name = node_map.getNamedItem("type");
 
-						if (attr_name != null && attr_name.getTextContent().equals("xsd:integer"))
+						if (attr_name != null && attr_name.getTextContent().equals("xsd:int"))
 							return true;
 					}
 				}
@@ -2646,7 +2646,7 @@ public class BMRxTool_Java {
 
 					Node attr_name = node_map.getNamedItem("base");
 
-					if (attr_name != null && attr_name.getTextContent().equals("xsd:integer"))
+					if (attr_name != null && attr_name.getTextContent().equals("xsd:int"))
 						return true;
 				}
 			}
@@ -2751,7 +2751,7 @@ public class BMRxTool_Java {
 					Node attr_type = node_map.getNamedItem("type");
 					Node attr_use = node_map.getNamedItem("use");
 
-					if (attr_name != null && attr_type != null && attr_type.getTextContent().equals("xsd:integer") && attr_use != null && attr_use.getTextContent().equals("required")) {
+					if (attr_name != null && attr_type != null && attr_type.getTextContent().equals("xsd:int") && attr_use != null && attr_use.getTextContent().equals("required")) {
 
 						String column_name = convert_attr_name(attr_name.getTextContent(), column_list);
 
@@ -3063,7 +3063,7 @@ public class BMRxTool_Java {
 
 					}
 
-					else if (attr_type.equals("xsd:integer")) {
+					else if (attr_type.equals("xsd:int")) {
 
 						// item='id'
 						if (attr_name_lower.equals("id"))
