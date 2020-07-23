a494ba4057aa49fc73271217c4ee09fc630c5a9a
Masashi Yokochi <yokochi47>
2020-07-23 20:41:41 +0900

    Merge

--- cs_complete/update_cs_complete.sh
+++ cs_complete/update_cs_complete.sh
@@ -2,17 +2,55 @@
 
 source ../scripts/db-user.sh
 
-java -classpath ../cs-complete.jar:../extlibs/* bmr_Util_Main --user-bmrb $DB_USER
+BMRB_MIRRORS=("www.bmrb.wisc.edu" "bmrb.pdbj.org") # "bmrb.cerm.unifi.it")
+
+printf "    BMRB mirror sites\t\t delay [ms]\n"
+echo "-------------------------------------------"
+
+BMRB_MIRROR=${BMRB_MIRRORS[0]}
+
+delay=10000
+i=1
+
+for url in ${BMRB_MIRRORS[@]}
+do
+
+ time=`ping -c 1 -w 10 $url | grep 'avg' | cut -d '=' -f 2 | cut -d '/' -f 2`
+
+ if [ $? = 0 ] ; then
+
+  printf "[%d] %s\t\t%6.1f\n" $i $url $time
+
+  cmp=`echo "$time > $delay" | bc 2> /dev/null`
+
+  if [ "$cmp" = 0 ] ; then
+
+   server_alive=`curl -I $url -m 5 2> /dev/null`
+
+   if [ $? == 0 ] ; then
+
+    BMRB_MIRROR=$url
+    delay=$time
+
+   fi
+
+  fi
+
+ else
+  echo $url: timed out.
+ fi
+
+ let i++
+
+done
+
+java -classpath ../cs-complete.jar:../extlibs/* bmr_Util_Main --user-bmrb $DB_USER --url-mirror $BMRB_MIRROR
 
 rm -rf cs_complete_loc
 
-if [ -e url_mirror ] ; then
+BMRB_PUB
 
- url_mirror=`cat url_mirror`
-
- rsync -av --delete cs_complete_str/* rsync://$url_mirror/cs-complete-internal
-
-fi
+rsync -av --delete cs_complete_str/* rsync://bmrbpub.pdbj.org/cs-complete-internal
 
 if [ ! `which psql` ] ; then
 
@@ -28,7 +66,7 @@ psql -U $DB_USER -l | grep $BMRB_DB > /dev/null || ( echo "database \"$BMRB_DB\"
 
 psql -d $BMRB_DB -U $DB_USER -f schema.cs_complete.sql
 
-java -classpath ../cs-complete.jar:../extlibs/* CS_complete --user-bmrb $DB_USER
+java -classpath ../cs-complete.jar:../extlibs/* CS_complete --user-bmrb $DB_USER --url-mirror $BMRB_MIRROR
 
 echo
 
